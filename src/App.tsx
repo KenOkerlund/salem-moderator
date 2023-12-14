71b745ddbae609133221233136449cc628e8a707
Ken Okerlund <ken.okerlund@gmail.com>
2023-12-13 20:40:00 -0700

    Initial homepage layout and design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import highlightedPerson from "./assets/images/highlighted-person.png";
+import ModeratorLogo from './assets/images/moderator-logo.svg';
 
 function App() {
-
-  return (
-    <>
-     
-    </>
-  )
+	return (
+		<main>
+			<div className='home'>
+				<img src={ModeratorLogo} alt="Salem Moderator logo" className='logo' />
+				<img src={highlightedPerson} alt="Lady" className='salem-lady' />
+				<h3>BEGIN</h3>
+				<h4>SYNC DEVICE</h4>
+			</div>
+		</main>
+	)
 }
 
 export default App
