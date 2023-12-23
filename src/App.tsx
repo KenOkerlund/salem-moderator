23e23afba1aba83a8cd1cd9336a5f5dea3dbf918
Ken Okerlund <ken.okerlund@gmail.com>
2023-12-23 09:36:34 -0700

    Installing and initial setup of BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './views/Home'
 
 function App() {
 	return (
 		<main>
-			<Home />
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+				</Routes>
+			</BrowserRouter>
 		</main>
 	)
 }
