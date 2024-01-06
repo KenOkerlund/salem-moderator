c17e4aca2a4911276d0ca527a051874d685071c9
Ken Okerlund <ken.okerlund@gmail.com>
2024-01-06 15:02:12 -0700

    Players object created and displaying

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,27 +1,45 @@
 import { useState } from 'react';
 
 function Settings() {
-    const [playerNames, setPlayerNames] = useState([
-        {
-            id: 1,
-            name: "Player 1",
-        },
-        {
-            id: 2,
-            name: "Player 2",
-        },
-        {
-            id: 3,
-            name: "Player 3",
-        },
-        {
-            id: 4,
-            name: "Player 4",
-        },
-    ])
+    function createPlayers() {
+        return [
+            {
+                id: '1',
+                name: "Player 1",
+            },
+            {
+                id: '2',
+                name: "Player 2",
+            },
+            {
+                id: '3',
+                name: "Player 3",
+            },
+            {
+                id: '4',
+                name: "Player 4",
+            },
+        ];
+    }
+    const [playerNames, setPlayerNames] = useState(createPlayers());
+
+
 
     return (
-        <input type="text" placeholder={playerNames[0].name}/>
+        <>
+            <div className='settings-players'>
+                <h6>PLAYERS</h6>
+            <div className='settings-players-names'>
+                {
+                    playerNames.map((playerName) => {
+                        return (
+                            <input type="text" id={playerName.id} placeholder={playerName.name} />
+                        )
+                    })
+                }
+            </div>
+            </div>
+        </>
     )
 }
 
