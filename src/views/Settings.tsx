43516e26519831ae2fcfde5431046b9631bd9c10
Ken Okerlund <ken.okerlund@gmail.com>
2024-01-15 18:53:09 -0700

    Further progress made on settings player object

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -23,21 +23,35 @@ function Settings() {
     }
     const [playerNames, setPlayerNames] = useState(createPlayers());
 
+    function handlePlayerNameChange(e: React.ChangeEvent<HTMLInputElement>, i: number) {
+        const copyPlayers = [...playerNames];
+        copyPlayers[i].name = e.target.value;
+        setPlayerNames(copyPlayers);
+        console.log('This is copyPlayers', copyPlayers[i].name)
+        console.log('This is playerNames', playerNames)
+
+    }
+
 
 
     return (
         <>
             <div className='settings-players'>
                 <h6>PLAYERS</h6>
-            <div className='settings-players-names'>
-                {
-                    playerNames.map((playerName) => {
-                        return (
-                            <input type="text" id={playerName.id} placeholder={playerName.name} />
-                        )
-                    })
-                }
-            </div>
+                <div className='settings-players-names'>
+                    {
+                        playerNames.map((playerName, i) => {
+                            return (
+                                <div key={playerName.id} className='settings-player-list'>
+                                    <p>-</p>
+                                    <input type="text" id={playerName.id} placeholder={playerName.name} onChange={(e) => handlePlayerNameChange(e, i)} />
+                                    <p>⬆⬇</p>
+                                </div>
+                            )
+                        })
+                    }
+                    <p>➕</p>
+                </div>
             </div>
         </>
     )
