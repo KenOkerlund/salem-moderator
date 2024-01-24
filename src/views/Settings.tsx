e6c9684a352f6008f3393ce6f4a4a3c639e153d9
Ken Okerlund <ken.okerlund@gmail.com>
2024-01-23 21:39:34 -0700

    Can now add, edit, and remove players. Still needs to be styled.

--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,38 +1,54 @@
 import { useState } from 'react';
+// import Button from '../elements/Button'
 
 function Settings() {
     function createPlayers() {
         return [
             {
-                id: '1',
+                id: 1,
                 name: "Player 1",
             },
             {
-                id: '2',
+                id: 2,
                 name: "Player 2",
             },
             {
-                id: '3',
+                id: 3,
                 name: "Player 3",
             },
             {
-                id: '4',
+                id: 4,
                 name: "Player 4",
             },
         ];
     }
     const [playerNames, setPlayerNames] = useState(createPlayers());
+    const [idForPlayerName, setIdForPlayerName] = useState(5);
+
+    const isThereLessThanTwelvePlayers = playerNames.length < 12;
+    const isThereMoreThanFourPlayers = playerNames.length > 4;
 
     function handlePlayerNameChange(e: React.ChangeEvent<HTMLInputElement>, i: number) {
         const copyPlayers = [...playerNames];
         copyPlayers[i].name = e.target.value;
         setPlayerNames(copyPlayers);
-        console.log('This is copyPlayers', copyPlayers[i].name)
-        console.log('This is playerNames', playerNames)
+        // console.log('This is copyPlayers', copyPlayers[i].name)
+        // console.log('This is playerNames', playerNames)
+    };
 
-    }
 
+    function addPlayerNameClick() {
+        const copyPlayers = [...playerNames];
+        copyPlayers.push({ id: idForPlayerName, name: `Player ${idForPlayerName}` });
+        setIdForPlayerName(idForPlayerName + 1)
+        setPlayerNames(copyPlayers);
+    };
 
+    function removePlayerNameClick(i: number) {
+        const copyPlayers = [...playerNames];
+        copyPlayers.splice(i, 1);
+        setPlayerNames(copyPlayers);
+    };
 
     return (
         <>
@@ -43,14 +59,13 @@ function Settings() {
                         playerNames.map((playerName, i) => {
                             return (
                                 <div key={playerName.id} className='settings-player-list'>
-                                    <p>-</p>
-                                    <input type="text" id={playerName.id} placeholder={playerName.name} onChange={(e) => handlePlayerNameChange(e, i)} />
-                                    <p>⬆⬇</p>
+                                    <button disabled={!isThereMoreThanFourPlayers} onClick={() => removePlayerNameClick(i)} >-</button>
+                                    <input type="text" placeholder={playerName.name} onChange={(e) => handlePlayerNameChange(e, i)} />
                                 </div>
                             )
                         })
                     }
-                    <p>➕</p>
+                    <button type='button' disabled={!isThereLessThanTwelvePlayers} onClick={() => addPlayerNameClick()}>➕</button>
                 </div>
             </div>
         </>
