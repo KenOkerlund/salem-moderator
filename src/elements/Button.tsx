957679e037c9bf6693669ab521c1cb8327d69338
Ken Okerlund <ken.okerlund@gmail.com>
2024-01-29 22:56:24 -0700

    Adding needed properties for the button

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -5,21 +5,24 @@ import './Button.css';
 type ButtonProps = {
     children: ReactNode;
     variation?: 'primary' | 'secondary';
-    size?: 'large' | 'small';
+    size?: 'large' | 'small' | 'mini';
     custom?: boolean;
+    onClick?: () => void;
+    disabled?: boolean;
 };
 
-function Button({ children, size, variation, custom }: ButtonProps) {
+function Button({ children, size, variation, custom, onClick, disabled }: ButtonProps) {
     let classes = "";
     if (!custom) {
         classes = clsx('button', {
             'button--large': size === 'large',
             'button--small': size === 'small',
+            'button--mini': size === 'mini',
             'button--borderless': variation === "secondary",
         })
     }
     return (
-        <button className={classes}>{children}</button>
+        <button className={classes} onClick={onClick} disabled={disabled}>{children}</button>
     )
 }
 
