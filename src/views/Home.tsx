9aa8e9841cf6e02a76bbc4ae0d3eaaae4adcf7c8
Ken Okerlund <ken.okerlund@gmail.com>
2024-01-29 22:56:52 -0700

    Added routing for the settings page

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import highlightedPerson from "../assets/images/highlighted-person.png";
 import ModeratorLogo from '../assets/images/moderator-logo.svg';
 import Button from "../elements/Button";
 
 function Home() {
+    const navigate = useNavigate();
+
+    function handleBeginClick() {
+        navigate('/settings/');
+    }
+
     return (
         <div className='home'>
             <img src={ModeratorLogo} alt="Salem Moderator logo" className='logo' />
             <img src={highlightedPerson} alt="Lady" className='salem-lady' />
-            <Button size="large">BEGIN</Button>
+            <Button size="large" onClick={handleBeginClick}>BEGIN</Button>
             <Button variation="secondary" size="small">SYNC DEVICE</Button>
         </div>
     )
