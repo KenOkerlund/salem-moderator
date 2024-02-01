23ac648fb462a08463af9a6037f47a419cf9d96d
Ken Okerlund <ken.okerlund@gmail.com>
2024-01-31 21:34:29 -0700

    Functionality for altering player order

--- src/views/Components/PlayerSettings.tsx
+++ src/views/Components/PlayerSettings.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Button from '../../elements/Button'
+import arrowButton from '../../assets/svg-icons/arrow.svg';
 
 function Settings() {
     function createPlayers() {
@@ -50,6 +51,20 @@ function Settings() {
         setPlayerNames(copyPlayers);
     };
 
+    function movePlayerDownClick(i: number) {
+        const copyPlayers = [...playerNames];
+        const elementMoving = copyPlayers.splice(i, 1);
+        copyPlayers.splice(i + 1, 0, elementMoving[0]);
+        setPlayerNames(copyPlayers);
+    };
+
+    function movePlayerUpClick(i: number) {
+        const copyPlayers = [...playerNames];
+        const elementMoving = copyPlayers.splice(i, 1);
+        copyPlayers.splice(i - 1, 0, elementMoving[0]);
+        setPlayerNames(copyPlayers);
+    };
+
 
     return (
         <>
@@ -64,9 +79,11 @@ function Settings() {
                                         <div className='icon--minus'></div>
                                     </Button>
                                     <input type="text" placeholder={playerName.name} onChange={(e) => handlePlayerNameChange(e, playerName.id)} />
-                                    <div className='settings-players-order'>
-                                        {!isThisTheLastPlayer(playerName.id) && <Button size='mini'>⬇</Button>}
-                                        {!isThisTheFirstPlayer(playerName.id) && <Button size='mini'>⬆</Button>}
+                                    <div className='settings-players-order-buttons'>
+                                        <div className='settings-players-order'>
+                                            {!isThisTheLastPlayer(playerName.id) && <Button size='mini' onClick={() => movePlayerDownClick(i)}>{<img src={arrowButton} className='down-arrow' />}</Button>}
+                                            {!isThisTheFirstPlayer(playerName.id) && <Button size='mini' onClick={() => movePlayerUpClick(i)}>{<img src={arrowButton} className='up-arrow' />}</Button>}
+                                        </div>
                                     </div>
                                 </div>
                             )
