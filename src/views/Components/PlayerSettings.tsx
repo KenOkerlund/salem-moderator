300e64d5bf04787e53957bb4d31f0ef5eb7ce6a2
Ken Okerlund <ken.okerlund@gmail.com>
2024-02-09 16:23:12 -0700

    Updated player settings to reflect the custom hook changes

--- src/views/Components/PlayerSettings.tsx
+++ src/views/Components/PlayerSettings.tsx
@@ -1,97 +1,49 @@
-import { useState } from 'react';
 import Button from '../../elements/Button';
 import arrowButton from '../../assets/svg-icons/arrow.svg';
 
-function createPlayers() {
-	return [
-		{
-			id: 0,
-			name: 'Player 1',
-		},
-		{
-			id: 1,
-			name: 'Player 2',
-		},
-		{
-			id: 2,
-			name: 'Player 3',
-		},
-		{
-			id: 3,
-			name: 'Player 4',
-		},
-	];
-}
+function Settings(props) {
+	const {
+		players,
+		renamePlayer,
+		addPlayer,
+		canAddPlayer,
+		removePlayer,
+		canRemovePlayer,
+		movePlayerDown,
+		movePlayerUp,
+	} = props;
 
-function Settings() {
-
-	const [players, setPlayers] = useState(createPlayers());
-
-	const isThereLessThanTwelvePlayers = players.length < 12;
-	const isThereMoreThanFourPlayers = players.length > 4;
 	const isThisTheFirstPlayer = (i: number) => i === players[0].id ? true : false;
 	const isThisTheLastPlayer = (i: number) => i === players[players.length - 1].id;
-
-	function handlePlayerNameChange(e: React.ChangeEvent<HTMLInputElement>, i: number) {
-		const copyPlayers = [...players];
-		copyPlayers[i].name = e.target.value;
-		setPlayers(copyPlayers);
-
-	}
-
-	function addPlayerNameClick() {
-		const copyPlayers = [...players];
-		const id = Math.max(...players.map(p => p.id)) + 1;
-		//player.name needs to be 1 value higher than player.id because the index starts at 0 and the name starts at "Player 1"
-		copyPlayers.push({ id, name: `Player ${id + 1} ` });
-		setPlayers(copyPlayers);
-	}
-
-	function removePlayerNameClick(i: number) {
-		const copyPlayers = [...players];
-		copyPlayers.splice(i, 1);
-		setPlayers(copyPlayers);
-	}
-
-	function movePlayerDownClick(i: number) {
-		const copyPlayers = [...players];
-		const elementMoving = copyPlayers.splice(i, 1);
-		copyPlayers.splice(i + 1, 0, elementMoving[0]);
-		setPlayers(copyPlayers);
-	}
-
-	function movePlayerUpClick(i: number) {
-		const copyPlayers = [...players];
-		const elementMoving = copyPlayers.splice(i, 1);
-		copyPlayers.splice(i - 1, 0, elementMoving[0]);
-		setPlayers(copyPlayers);
-	}
-
 
 	return (
 		<div className='settings-players'>
 			<h6>PLAYERS</h6>
 			<div className='settings-players-names'>
 				{
-					players.map((playerName, i) => {
+					players.map((player) => {
 						return (
-							<div key={playerName.id} className='settings-player-list'>
-								<Button size='mini' disabled={!isThereMoreThanFourPlayers} onClick={() => removePlayerNameClick(i)}>
+							<div key={player.id} className='settings-player-list'>
+								<Button
+									size='mini'
+									disabled={!canRemovePlayer}
+									onClick={() => removePlayer(player.id)}
+								>
 									<div className='icon--minus'></div>
 								</Button>
-								<input type="text" placeholder={playerName.name} onChange={(e) => handlePlayerNameChange(e, playerName.id)} />
+								<input type="text" placeholder={`Player ${player.id + 1}`} onChange={(e) => renamePlayer(e, player.id)} value={player.name} />
 								<div className='settings-players-order-buttons'>
 									<div className='settings-players-order'>
-										{!isThisTheLastPlayer(playerName.id) && <Button size='mini' onClick={() => movePlayerDownClick(i)}>{<img src={arrowButton} className='down-arrow' />}</Button>}
-										{!isThisTheFirstPlayer(playerName.id) && <Button size='mini' onClick={() => movePlayerUpClick(i)}>{<img src={arrowButton} className='up-arrow' />}</Button>}
+										{!isThisTheLastPlayer(player.id) && <Button size='mini' onClick={() => movePlayerDown(player.id)}>{<img src={arrowButton} className='down-arrow' />}</Button>}
+										{!isThisTheFirstPlayer(player.id) && <Button size='mini' onClick={() => movePlayerUp(player.id)}>{<img src={arrowButton} className='up-arrow' />}</Button>}
 									</div>
 								</div>
 							</div>
 						);
 					})
 				}
-				{isThereLessThanTwelvePlayers && <div className='settings-player-add'>
-					<Button size="mini" disabled={!isThereLessThanTwelvePlayers} onClick={() => addPlayerNameClick()}>
+				{canAddPlayer && <div className='settings-player-add'>
+					<Button size="mini" onClick={addPlayer}>
 						<div className='icon--plus'></div>
 					</Button>
 				</div>}
--- src/views/Settings.tsx
+++ src/views/Settings.tsx
@@ -1,9 +1,35 @@
 import PlayersList from './Components/PlayerSettings';
+import ResetSettings from './Components/ResetSettings';
+import useSettings from './use-settings';
 
 function Settings() {
+
+	const {
+		players,
+		changePlayerName,
+		addPlayer,
+		canAddPlayer,
+		removePlayer,
+		canRemovePlayer,
+		movePlayerDown,
+		movePlayerUp,
+		resetPlayers
+	} = useSettings();
+
+	console.log(players);
 	return (
 		<div className="flex-column">
-			<PlayersList />
+			<PlayersList
+				players={players}
+				renamePlayer={changePlayerName}
+				addPlayer={addPlayer}
+				canAddPlayer={canAddPlayer}
+				removePlayer={removePlayer}
+				canRemovePlayer={canRemovePlayer}
+				movePlayerDown={movePlayerDown}
+				movePlayerUp={movePlayerUp}
+			/>
+			<ResetSettings onReset={resetPlayers} />
 		</div>
 	);
 }
