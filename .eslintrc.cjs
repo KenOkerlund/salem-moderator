7db71d6e2aa1e01026ff41415bae27881c54cf5d
Kevin Okerlund <kevinokerlund@users.noreply.github.com>
2024-02-11 00:19:01 -0700

    specify vitest in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, vitest: true },
   settings: {
     react: {
       version: "detect"
