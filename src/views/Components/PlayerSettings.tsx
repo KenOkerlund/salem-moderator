103b9b293cbc9c35749cc39ff32b84c64002aef0
Kevin Okerlund <kevinokerlund@users.noreply.github.com>
2024-04-15 00:01:33 -0600

    use components and module css for player settings

--- src/views/Components/PlayerSettings.tsx
+++ src/views/Components/PlayerSettings.tsx
@@ -1,6 +1,10 @@
 import Button from '../../elements/Button';
 import arrowButton from '../../assets/svg-icons/arrow.svg';
 import useSettings from '../use-settings';
+import styles from './player-settings.module.css';
+import { TextInput } from '../../elements/input/text-input';
+import { IconMinus } from '../../assets/html-css-icons/icon-minus';
+import { IconPlus } from '../../assets/html-css-icons/icon-plus';
 
 function Settings(props: Omit<ReturnType<typeof useSettings>, 'resetPlayers'>) {
 	const {
@@ -18,34 +22,50 @@ function Settings(props: Omit<ReturnType<typeof useSettings>, 'resetPlayers'>) {
 	const isThisTheLastPlayer = (i: number) => i === players[players.length - 1].id;
 
 	return (
-		<div className='settings-players'>
-			<h6>PLAYERS</h6>
-			<div className='settings-players-names'>
+		<div className={styles.players}>
+			<h2>PLAYERS</h2>
+			<div className={styles.playerNames}>
 				{
 					players.map((player) => {
 						return (
-							<div key={player.id} className='settings-player-list'>
+							<div key={player.id} className={styles.playerName}>
 								<Button
-									size='mini'
+									iconOnly
+									size='small'
 									disabled={!canRemovePlayer}
 									onClick={() => removePlayer(player.id)}
 								>
-									<div className='icon--minus'></div>
+									<IconMinus />
 								</Button>
-								<input type="text" placeholder={`Player ${player.id + 1}`} onChange={(e) => changePlayerName(e, player.id)} value={player.name} />
-								<div className='settings-players-order-buttons'>
-									<div className='settings-players-order'>
-										{!isThisTheLastPlayer(player.id) && <Button size='mini' onClick={() => movePlayerDown(player.id)}>{<img src={arrowButton} className='down-arrow' />}</Button>}
-										{!isThisTheFirstPlayer(player.id) && <Button size='mini' onClick={() => movePlayerUp(player.id)}>{<img src={arrowButton} className='up-arrow' />}</Button>}
-									</div>
-								</div>
+								<TextInput
+									placeholder={`Player ${player.id + 1}`}
+									onChange={(e) => changePlayerName(e, player.id)} value={player.name}
+								/>
+								<Button
+									iconOnly
+									size="small"
+									onClick={() => movePlayerDown(player.id)}
+									disabled={isThisTheLastPlayer(player.id)}
+								>
+									<img src={arrowButton} />
+								</Button>
+
+								<Button
+									iconOnly
+									rotateContent
+									size="small"
+									onClick={() => movePlayerUp(player.id)}
+									disabled={isThisTheFirstPlayer(player.id)}
+								>
+									<img src={arrowButton} />
+								</Button>
 							</div>
 						);
 					})
 				}
-				{canAddPlayer && <div className='settings-player-add'>
-					<Button size="mini" onClick={addPlayer}>
-						<div className='icon--plus'></div>
+				{canAddPlayer && <div>
+					<Button size="small" onClick={addPlayer}>
+						<IconPlus /> Add Player
 					</Button>
 				</div>}
 			</div>
