1856bc8a8809209330ecb878cbb16d6d45666341
Ken Okerlund <ken.okerlund@gmail.com>
2024-04-22 22:39:44 -0600

    Phase selection page remade

--- src/views/PhaseSelection.tsx
+++ src/views/PhaseSelection.tsx
@@ -1,37 +1,42 @@
+import Button from '../elements/Button';
+import arrowButton from '../assets/svg-icons/arrow.svg';
 import SettingsButton from '../elements/SettingsButton';
-import dawnTile from '../assets/images/dawn-tile.png';
-import nightTile from '../assets/images/night-tile.png';
+import styles from './phase-selection.module.css';
 
 function PhaseSelection(){
 	return (
-		<>
+		<div className={styles.phasePage}>
 			<SettingsButton />
-			<div className="phase-selection">
-				<div className='phase-card dawn-card'>
-					<div>
-						<h6>DAWN</h6>
-						<p>The witches select a player to receive the black cat.</p>
-					</div>
-					<img src={dawnTile} alt="Picture of a black cat" />
-				</div>
-				<div className='night-and-constable-card'>
-					<div className='phase-card night-card'>
-						<div>
-							<h6>NIGHT</h6>
-							<div className='night-card-content'>
-								<p>The witches select a player they wish to kill.</p>
-								<p>The constable selects a player to attempt to save.</p>
-							</div>
-						</div>
-						<img src={nightTile} alt="Picture of a black cats eyes" />
-					</div>
-					<div className='night-phase-constable'>
-						<input type="checkbox" name="constable" id="constable" />
-						<label htmlFor="constable">CONSTABLE</label>
-					</div>
+
+			<div className={styles.phaseType + ' ' + styles.dawnPhase}>
+				<h1 className={styles.header}>Dawn</h1>
+				<p>The witches select a player to receive the black cat.</p>
+				<div>
+					<Button size='small'>
+						Begin 
+						<img src={arrowButton} className={styles.arrowIcon} /> 
+					</Button>
 				</div>
 			</div>
-		</>
+
+			<div className={styles.line} />
+
+			<div className={styles.phaseType + ' ' + styles.nightPhase}>
+				<h1 className={styles.header}>Night</h1>
+				<p>The witches select a player they wish to kill.</p>
+				<p>The constable selects a player to attempt to save.</p>
+				<div>
+					<Button size='small'>
+						Begin 
+						<img src={arrowButton} className={styles.arrowIcon} /> 
+					</Button>
+				</div>
+				<div className={styles.constable}>
+					<input type="checkbox" id='constable' value="constable" name='constable' />
+					<label htmlFor="constable">Constable</label>
+				</div>
+			</div>
+		</div>
 	);
 }
 
