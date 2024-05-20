47b5d493d0b826f356b8f697fcd4187bc29877fe
Ken <ken.okerlund@gmail.com>
2024-05-20 17:03:38 -0600

    Updated the phase page and individual phases to dynamically render. Starter css added.

--- src/views/Components/Phase.tsx
+++ src/views/Components/Phase.tsx
@@ -3,9 +3,10 @@ import { Footer } from './footer/footer';
 import styles from './phase.module.css';
 
 type phaseProps = {
-    phaseType: 'Dawn' | 'Night';
-    children: ReactNode;
-    secondaryButton?: boolean;
+	phaseType: 'Dawn' | 'Night';
+	children: ReactNode;
+	secondaryButton?: boolean;
+	secondaryButtonClick?: () => void;
 }
 
 export function Phase(props: phaseProps) {
@@ -13,17 +14,15 @@ export function Phase(props: phaseProps) {
 
 	}
 
-	function nextButtonClick() {
 
-	}
-
-	return (
-		<>
+	return (<>
+		<div className={styles.phasePage}>
+			<h1 className={styles.phaseType}>{props.phaseType}</h1>
 			<div className={styles.content}>
-				<h1>{props.phaseType}</h1>
 				<div>{props.children}</div>
 			</div>
-			<Footer primaryButtonText='Abort' onPrimaryClick={abortButtonClick} secondaryButtonText={props.secondaryButton ? 'Next' : undefined } onSecondaryClick={nextButtonClick} />
-		</>
+		</div>
+			<Footer primaryButtonText='Abort' onPrimaryClick={abortButtonClick} secondaryButtonText={props.secondaryButton ? 'Next' : undefined} onSecondaryClick={props.secondaryButtonClick} />
+			</>
 	);
 }
--- src/views/PhaseSelection.tsx
+++ src/views/PhaseSelection.tsx
@@ -1,27 +1,39 @@
 import { useState } from 'react';
+import { set } from 'lodash';
 import Button from '../elements/Button';
 import arrowButton from '../assets/svg-icons/arrow.svg';
 import SettingsButton from '../elements/SettingsButton';
 import styles from './phase-selection.module.css';
 import { Phase } from './Components/Phase';
 
-function PhaseSelection(){
-	const [phase, setPhase] = useState({
-		phaseSelection: true,
-		dawn: {
-			blackCatInstructions: false,
-			blackCatAssignment: false,
-			blackCatReveal: false,
-		},
-		night: {
-			nightInstructions: false,
-			deathAssignment: false,
-			constableSave: false,
-			constableReveal: false,
-			confession: false,
-			deathReveal: false,
-		},
-	});
+function PhaseSelection() {
+	function createState() {
+		return {
+			phaseSelection: true,
+			dawn: {
+				blackCatInstructions: false,
+				blackCatAssignment: false,
+				blackCatReveal: false,
+			},
+			night: {
+				nightInstructions: false,
+				deathAssignment: false,
+				constableSave: false,
+				constableReveal: false,
+				confession: false,
+				deathReveal: false,
+			},
+		}
+	};
+	const [phase, setPhase] = useState(createState());
+	const [isConstableChecked, setIsConstableChecked] = useState(true);
+
+	function updatePhase(stage: string) {
+		let tempState = createState();
+		tempState.phaseSelection = false;
+		set(tempState, stage, true);
+		setPhase(tempState);
+	};
 
 	return (
 		<>
@@ -32,9 +44,9 @@ function PhaseSelection(){
 					<h1 className={styles.header}>Dawn</h1>
 					<p>The witches select a player to receive the black cat.</p>
 					<div>
-						<Button size='small'>
-						Begin 
-							<img src={arrowButton} className={styles.arrowIcon} /> 
+						<Button size='small' onClick={() => updatePhase('dawn.blackCatInstructions')}>
+							Begin
+							<img src={arrowButton} className={styles.arrowIcon} />
 						</Button>
 					</div>
 				</div>
@@ -46,28 +58,28 @@ function PhaseSelection(){
 					<p>The witches select a player they wish to kill.</p>
 					<p>The constable selects a player to attempt to save.</p>
 					<div>
-						<Button size='small'>
-						Begin 
-							<img src={arrowButton} className={styles.arrowIcon} /> 
+						<Button size='small' onClick={() => updatePhase('night.nightInstructions')}>
+							Begin
+							<img src={arrowButton} className={styles.arrowIcon} />
 						</Button>
 					</div>
 					<div className={styles.constable}>
-						<input type="checkbox" id='constable' value="constable" name='constable' />
+						<input type="checkbox" id='constable' value="constable" name='constable' checked={isConstableChecked} onChange={() => setIsConstableChecked(!isConstableChecked)} />
 						<label htmlFor="constable">Constable</label>
 					</div>
 				</div>
-			</div> }
+			</div>}
 
-			{phase.dawn.blackCatInstructions && <Phase phaseType='Dawn'>Instructions with sound</Phase>}
-			{phase.dawn.blackCatAssignment && <Phase phaseType='Dawn'>Text instructions and cards to confirm selection</Phase>}
-			{phase.dawn.blackCatReveal && <Phase phaseType='Dawn'>Text prompt and card to reveal</Phase>}
+			{phase.dawn.blackCatInstructions && <Phase phaseType='Dawn' secondaryButton secondaryButtonClick={() => updatePhase('dawn.blackCatAssignment')}>Audio instructions</Phase>}
+			{phase.dawn.blackCatAssignment && <Phase phaseType='Dawn' secondaryButton secondaryButtonClick={() => updatePhase('dawn.blackCatReveal')}>Text instructions and cards to confirm selection</Phase>}
+			{phase.dawn.blackCatReveal && <Phase phaseType='Dawn' secondaryButton secondaryButtonClick={() => updatePhase('phaseSelection')}>Text prompt and card to reveal</Phase>}
 
-			{phase.night.nightInstructions && <Phase phaseType='Night'>Instructions with sound</Phase>}
-			{phase.night.deathAssignment && <Phase phaseType='Night'>Text instructions and cards to select player</Phase>}
-			{phase.night.constableSave && <Phase phaseType='Night'>Text instructions and cards for constable to attempt save</Phase>}
-			{phase.night.constableReveal && <Phase phaseType='Night'>Text guide and card for revealing who the constable tried to save</Phase>}
-			{phase.night.confession && <Phase phaseType='Night'>Text guide for people to decide to confess</Phase>}
-			{phase.night.deathReveal && <Phase phaseType='Night'>Text guide with card to reveal the potentially dead player</Phase>}
+			{phase.night.nightInstructions && <Phase phaseType='Night' secondaryButton secondaryButtonClick={() => updatePhase('night.deathAssignment')}>Instructions with sound</Phase>}
+			{phase.night.deathAssignment && <Phase phaseType='Night' secondaryButton secondaryButtonClick={isConstableChecked ? () => updatePhase('night.constableSave') : () => updatePhase('night.confession')}>Text instructions and cards to select player</Phase>}
+			{phase.night.constableSave && <Phase phaseType='Night' secondaryButton secondaryButtonClick={() => updatePhase('night.constableReveal')}>Text instructions and cards for constable to attempt save</Phase>}
+			{phase.night.constableReveal && <Phase phaseType='Night' secondaryButton secondaryButtonClick={() => updatePhase('night.confession')}>Text guide and card for revealing who the constable tried to save</Phase>}
+			{phase.night.confession && <Phase phaseType='Night' secondaryButton secondaryButtonClick={() => updatePhase('night.deathReveal')}>Text guide for people to decide to confess</Phase>}
+			{phase.night.deathReveal && <Phase phaseType='Night' secondaryButton secondaryButtonClick={() => updatePhase('phaseSelection')}>Text guide with card to reveal the potentially dead player</Phase>}
 		</>
 	);
 }
