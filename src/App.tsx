dba7177c0c05d3dfba84a5dda0fa746d680c2643
Ken Okerlund <ken.okerlund@gmail.com>
2024-07-16 23:08:35 -0600

    Updated component to compensate for the renamed component call

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './views/Home';
 import Settings from './views/Settings';
-import PhaseSelection from './views/PhaseSelection';
+import { PhaseSelection } from './views/phase-selection';
 import styles from './app.module.css';
 import { PlayersContext, createPlayers } from './players-context';
 import { useState } from 'react';
