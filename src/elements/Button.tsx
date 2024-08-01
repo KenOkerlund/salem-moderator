806c86aff30280aaa0386c111ce1720100dd6ea7
Ken Okerlund <ken.okerlund@gmail.com>
2024-07-31 22:41:14 -0600

    Updates to the button component to allow for press-and-hold

--- src/elements/Button.tsx
+++ src/elements/Button.tsx
@@ -1,14 +1,15 @@
-import { ReactNode } from 'react';
+import { ReactNode, useRef, useState } from 'react';
 import clsx from 'clsx';
 import styles from './button.module.css';
 
 type ButtonProps = {
 	children: ReactNode;
 	variation?: 'primary' | 'secondary';
-	size?: 'small' | 'medium' | 'large';
+	size?: 'small' | 'medium' | 'large' | 'largest';
 	onClick?: () => void;
+	holdDuration?: number;
 	disabled?: boolean;
-	width?: 100;
+	width?: number;
 	iconOnly?: boolean;
 	rotateContent?: boolean;
 };
@@ -23,25 +24,53 @@ function Button(props: ButtonProps) {
 		width,
 		iconOnly = false,
 		rotateContent,
+		holdDuration,
 	} = props;
 
+	const [isHeld, setIsHeld] = useState(false);
+	const holdTimeoutRef = useRef<NodeJS.Timeout | null>(null);
 
 	const classes = clsx(styles.button, {
 		[styles.small]: size === 'small',
 		[styles.medium]: size === 'medium',
 		[styles.large]: size === 'large',
+		[styles.largest]: size === 'largest',
 		[styles.secondary]: variation === 'secondary',
 		[styles.fullWidth]: width === 100,
 		[styles.iconOnly]: iconOnly,
 		[styles.disabled]: disabled,
 		[styles.rotateContent]: rotateContent,
+		[styles.holdAnimation]: holdDuration && isHeld,
 	});
+
+	const handleMouseDown = () => {
+		setIsHeld(true);
+		holdDuration && (holdTimeoutRef.current = setTimeout(() => {
+			onClick && onClick();
+		}, holdDuration * 1000));
+	};
+
+	const handleMouseUp = () => {
+		setIsHeld(false);
+		holdTimeoutRef.current && clearTimeout(holdTimeoutRef.current);
+	};
 
 	return (
 		<button
+			style={{
+				animationDuration: holdDuration ? `${holdDuration}s` : undefined,
+			}}
+			onMouseDown={handleMouseDown}
+			onMouseUp={handleMouseUp}
+			onMouseLeave={handleMouseUp}
+			onDragLeave={handleMouseUp}
+			onTouchStart={handleMouseDown}
+			onTouchMove={handleMouseUp}
+			onTouchEnd={handleMouseUp}
 			className={classes}
-			onClick={onClick}
-			disabled={disabled}>
+			onClick={!holdDuration ? onClick : undefined}
+			disabled={disabled}
+		>
 			{children}
 		</button>
 	);
