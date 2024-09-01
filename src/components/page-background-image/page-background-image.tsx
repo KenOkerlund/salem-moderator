2170113319a3717a9d8921ab368cb4e65f9279b7
Ken Okerlund <ken.okerlund@gmail.com>
2024-09-01 17:12:45 -0600

    New background image component and styles

---
+++ src/components/page-background-image/page-background-image.tsx
@@ -0,0 +1,27 @@
+import clsx from 'clsx';
+import styles from './page-background-image.module.css';
+
+type PageBackgroundImageProps = {
+	image: string;
+	shrink?: boolean;
+	fitImage?: boolean;
+};
+
+export default function PageBackgroundImage(props: PageBackgroundImageProps) {
+	const { image, shrink, fitImage } = props;
+
+	const imageClasses = clsx({
+		[styles.shrinkImage]: shrink,
+		[styles.fitImage]: fitImage,
+	});
+
+	return (
+		<div className={styles.container}>
+			<img
+				className={imageClasses}
+				src={image}
+				alt='testing cat eyes'
+			/>
+		</div>
+	);
+}
