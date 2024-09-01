10dfe2b0327135e2ebd4c0a305085997f64e991b
Ken Okerlund <ken.okerlund@gmail.com>
2024-09-01 17:12:59 -0600

    Applying new component to the home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,11 @@
 import { useNavigate } from 'react-router-dom';
+
 import Button from '../../components/button/button';
+import PageBackgroundImage from '../../components/page-background-image/page-background-image';
 
 import highlightedPerson from '../../assets/images/highlighted-person.png';
 import ModeratorLogo from '../../assets/images/moderator-logo.svg';
+import catEyes from '../../assets/images/cat-eyesl.png';
 
 import styles from './Home.module.css';
 
@@ -11,20 +14,28 @@ function Home() {
 
 	return (
 		<div className={styles.home}>
+			<PageBackgroundImage
+				image={catEyes}
+				shrink
+			/>
 			<div className={styles.container}>
 				<img
 					src={ModeratorLogo}
-					alt="Salem Moderator logo"
+					alt='Salem Moderator logo'
 					className={styles.logo}
 				/>
 
 				<img
 					src={highlightedPerson}
-					alt="Lady"
+					alt='Lady'
 					className={styles.person}
 				/>
-				
-				<Button width={100} size="large" onClick={() => navigate('/settings/')}>
+
+				<Button
+					width={100}
+					size='large'
+					onClick={() => navigate('/settings/')}
+				>
 					BEGIN
 				</Button>
 				{/* <Button variation='secondary'>SYNC DEVICE</Button> */}
