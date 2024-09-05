45ee10fc460b486450f6fea55a0d5ed09086b809
Ken Okerlund <ken.okerlund@gmail.com>
2024-09-04 22:04:18 -0600

    Update to the settings button component to allow for redirect from the selection page

--- src/pages/shared-components/settings-button/settings-button.tsx
+++ src/pages/shared-components/settings-button/settings-button.tsx
@@ -1,8 +1,26 @@
-import classes from './settings-button.module.css';
+import { useNavigate } from 'react-router-dom';
+
+import Button from '../../../components/button/button';
 import settingsIcon from '../../../assets/svg-icons/settings.svg';
-function SettingsButton(){
+
+import classes from './settings-button.module.css';
+
+function SettingsButton() {
+	const navigate = useNavigate();
+
 	return (
-		<img className={classes.icon} src={settingsIcon} alt="gear icon" />
+		<div className={classes.icon}>
+			<Button
+				iconOnly
+				onClick={() => navigate('/settings/')}
+				variation='secondary'
+			>
+				<img
+					src={settingsIcon}
+					alt='gear icon'
+				/>
+			</Button>
+		</div>
 	);
 }
 
