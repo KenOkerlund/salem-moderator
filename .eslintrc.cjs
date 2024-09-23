c1c954f7d37f5a6cb14d7f7791a4d95a385dd03f
Ken Okerlund <ken.okerlund@gmail.com>
2024-09-22 21:42:15 -0600

    edit eslint rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,7 +27,7 @@ module.exports = {
 			{ allowConstantExport: true },
 		],
 		indent: ['error', 'tab'],
-		quotes: ['error', 'single'],
+		quotes: ['error', 'single', {'avoid': 'escape'}],
 		'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
 		semi: ['error', 'always'],
 		"comma-dangle": [
