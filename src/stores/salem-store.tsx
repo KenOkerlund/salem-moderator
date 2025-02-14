a141d49482314bb15b6e7bb8e1a55b25af10b341
Ken Okerlund <ken.okerlund@gmail.com>
2025-02-13 22:48:23 -0700

    Updating salem-store to use zuhstands persist

--- src/stores/salem-store.tsx
+++ src/stores/salem-store.tsx
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { Player } from '../types';
 
 type SalemState = {
@@ -15,8 +16,6 @@ type SalemState = {
 
 export const maxPlayers = 12;
 export const minPlayers = 4;
-
-export const salemPlayerLocalStorageKey = 'salem_players';
 
 export function createPlayers(): Player[] {
 	return [
@@ -39,85 +38,63 @@ export function createPlayers(): Player[] {
 	];
 }
 
-export function getOrCreatePlayers(): Player[] {
-	const possibleStoredSalemPlayer = window.localStorage.getItem(salemPlayerLocalStorageKey);
-	if (possibleStoredSalemPlayer) {
-		try {
-			const players = JSON.parse(possibleStoredSalemPlayer);
-
-			if (Array.isArray(players) && players.length > 3) {
-				const eachIsPlayer = players.every((item) => 'id' in item && 'name' in item);
-				if (eachIsPlayer) {
-					return players;
-				}
+export const useSalemStore = create<SalemState>()(
+	persist(
+		(set) => (
+			{
+				instructionSpeech: true,
+				setInstructionSpeech: (enabled) => set(() => ({ instructionSpeech: enabled })),
+				players: createPlayers(),
+				setPlayerName: (playerId, name) => 
+					set((state) => {
+						const players = [...state.players];
+						const playerIndex = players.findIndex((player) => player.id === playerId);
+						if (playerIndex >= 0) {
+							players[playerIndex].name = name;
+						}
+						return { players };
+					}),
+				resetPlayers: () => {
+					set(() => ({ players: createPlayers() }));
+				},
+				addPlayer: () => set((state) => {
+					if (state.players.length >= maxPlayers) {
+						return state;
+					}
+					const copyPlayers = [...state.players];
+					const id = Math.max(...copyPlayers.map(p => p.id)) + 1;
+					//player.name needs to be 1 value higher than player.id because the index starts at 0 and the name starts at "Player 1"
+					copyPlayers.push({ id, name: '' });
+					return { players: copyPlayers };
+				}),
+				removePlayer: (playerId) => set((state) => {
+					if (state.players.length <= minPlayers) {
+						return state;
+					}
+					const copyPlayers = state.players.filter(p => p.id !== playerId);
+					return { players: copyPlayers };
+				}),
+				movePlayerDown: (playerId) => set((state) => {
+					const copyPlayers = [...state.players];
+					const playerIndex = copyPlayers.findIndex(p => p.id === playerId);
+					const elementMoving = copyPlayers.splice(playerIndex, 1);
+					copyPlayers.splice(playerIndex + 1, 0, elementMoving[0]);
+					return { players: copyPlayers };
+				}),
+				movePlayerUp: (playerId) => set((state) => {
+					const copyPlayers = [...state.players];
+					const playerIndex = copyPlayers.findIndex(p => p.id === playerId);
+					const elementMoving = copyPlayers.splice(playerIndex, 1);
+					copyPlayers.splice(playerIndex - 1, 0, elementMoving[0]);
+					return { players: copyPlayers };
+				}),
 			}
-		} catch (e) {
-			// Clean up whatever existed here that was bad...
-			window.localStorage.removeItem(salemPlayerLocalStorageKey);
-		}
-	}
-
-	return createPlayers();
-}
-
-function savePlayersToLocalStorage(players: Player[]){
-	window.localStorage.setItem(salemPlayerLocalStorageKey, JSON.stringify(players));
-}
-
-export const useSalemStore = create<SalemState>((set) => ({
-	instructionSpeech: true,
-	setInstructionSpeech: (enabled) => set(() => ({ instructionSpeech: enabled })),
-	players: getOrCreatePlayers(),
-	setPlayerName: (playerId, name) => 
-		set((state) => {
-			const players = [...state.players];
-			const playerIndex = players.findIndex((player) => player.id === playerId);
-			if (playerIndex >= 0) {
-				players[playerIndex].name = name;
-			}
-			savePlayersToLocalStorage(players);
-			return { players };
-		}),
-	resetPlayers: () => {
-		set(() => ({ players: createPlayers() }));
-		window.localStorage.removeItem(salemPlayerLocalStorageKey);
-	},
-	addPlayer: () => set((state) => {
-		if (state.players.length >= maxPlayers) {
-			return state;
-		}
-		const copyPlayers = [...state.players];
-		const id = Math.max(...copyPlayers.map(p => p.id)) + 1;
-		//player.name needs to be 1 value higher than player.id because the index starts at 0 and the name starts at "Player 1"
-		copyPlayers.push({ id, name: '' });
-		savePlayersToLocalStorage(copyPlayers);
-		return { players: copyPlayers };
-	}),
-	removePlayer: (playerId) => set((state) => {
-		if (state.players.length <= minPlayers) {
-			return state;
-		}
-		const copyPlayers = state.players.filter(p => p.id !== playerId);
-		savePlayersToLocalStorage(copyPlayers);
-		return { players: copyPlayers };
-	}),
-	movePlayerDown: (playerId) => set((state) => {
-		const copyPlayers = [...state.players];
-		const playerIndex = copyPlayers.findIndex(p => p.id === playerId);
-		const elementMoving = copyPlayers.splice(playerIndex, 1);
-		copyPlayers.splice(playerIndex + 1, 0, elementMoving[0]);
-		savePlayersToLocalStorage(copyPlayers);
-		return { players: copyPlayers };
-	}),
-	movePlayerUp: (playerId) => set((state) => {
-		const copyPlayers = [...state.players];
-		const playerIndex = copyPlayers.findIndex(p => p.id === playerId);
-		const elementMoving = copyPlayers.splice(playerIndex, 1);
-		copyPlayers.splice(playerIndex - 1, 0, elementMoving[0]);
-		savePlayersToLocalStorage(copyPlayers);
-		return { players: copyPlayers };
-	}),
-}));
+		),
+		{
+			name: 'salem_storage',
+		},
+	),
+);
 
 export const useCanAddPlayer = () => useSalemStore((state) => state.players.length < maxPlayers);
 export const useCanRemovePlayer = () => useSalemStore((state) => state.players.length > minPlayers);
