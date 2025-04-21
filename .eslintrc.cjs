c14fba84eb574e60b637c0208ee3922cf431b231
Ken Okerlund <ken.okerlund@gmail.com>
2025-04-21 12:59:01 -0600

    Updates

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2020: true,
+		'vitest-globals/env': true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'plugin:vitest-globals/recommended',
+	],
+	ignorePatterns: ['dist', '.eslintrc.cjs'],
+	parser: '@typescript-eslint/parser',
+	plugins: ['react-refresh', 'vitest'],
+	rules: {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		],
+		// If this is enabled it will conflict with prettier
+		// indent: ['error', 'tab'],
+		quotes: ['error', 'single', { avoidEscape: true }],
+		'linebreak-style': [
+			'error',
+			process.platform === 'win32' ? 'windows' : 'unix',
+		],
+		semi: ['error', 'always'],
+		'comma-dangle': [
+			'error',
+			{
+				arrays: 'always-multiline',
+				objects: 'always-multiline',
+				imports: 'always-multiline',
+				exports: 'always-multiline',
+				functions: 'always-multiline',
+			},
+		],
+		'eol-last': ['error', 'always'],
+		'object-curly-spacing': ['error', 'always'],
+		'array-bracket-spacing': ['error', 'never'],
+		'comma-spacing': ['error', { after: true, before: false }],
+		'no-unneeded-ternary': 'error',
+	},
+};
